0a1794b37b1d93afccc607e106ff196ac13bdfce
Temmerman <temmermans@delawareconsulting.com>
2016-06-05 16:01:53 +0200

    complete basic security setup

--- security/security-setup.js
+++ security/security-setup.js
@@ -2,17 +2,61 @@ var express = require('express');
 var enforceSSL = require('express-enforces-ssl');
 var helmet = require('helmet');
 var ms = require('ms');
+var csrf = require('csurf');
 var app = express();
 
 //Make sure all requests use HTTPS instead of HTTP. We need to enbale the trust proxy
 //because when deploying to Heroku, Heroku is between the server and the client. So we need to let out app now about this.
 //we use helmet to keep them on HTTPS and not go back to HTTP
-app.enable('trust proxy');
-app.use(enforceSSL());
 
-app.use(helmet.hsts({
-  maxAge: ms('1 year'),
-  includeSubdomains: true
-}));
+
+// app.enable('trust proxy');
+// app.use(enforceSSL());
+//
+// app.use(helmet.hsts({
+//   maxAge: ms('1 year'),
+//   includeSubdomains: true
+// }));
+
+//Protect against reflected XSS
+app.use(helmet.xssFilter());
+
+//Protect against cross forgery attacks by using the csurf module. This will add a .csrfToken to the req body
+//If the request is something other than a GET we can look to a parameter _csrf to validate the request.
+//This can be done in the following way:
+//
+// app.get("/", function(req, res) {
+//   res.render("myview", {
+//     csrfToken: req.csrfToken()
+//   });
+// });
+//
+// And in the view:
+//
+// <form method="post" action="/submit">
+//   <input name="_csrf" value="<%= csrfToken %>" type="hidden">
+//   <! -- ... -->
+// </form>
+//
+//The rest will be handled by csurf. It is not required but it is best to have some kind of handler for failed CSRF requests catching the CSRF error.
+//
+// app.use(function(err, req, res, next) {
+//   if (err.code !== "EBADCSRFTOKEN") {
+//     next(err);
+//     return;
+//   }
+//   res.status(403);
+//   res.send("CSRF error.");
+// });
+app.use(csrf());
+
+//disable the header so its not immediatly clear that this is an express application
+app.disable("x-powered-by");
+
+//prevent clickjacking
+app.use(helmet.frameguard("sameorigin"));
+
+//keeping adobe products out of the site (like flash)
+app.use(helmet.noSniff());
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
 
 //set Jade as default templating engine
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 
+//the default store is not for production usage. Install a store compatible with the db you are using from: https://github.com/expressjs/session#compatible-session-stores
+//this is also a dependency of csurf
+app.use(session({
+  secret: "@lHJr+JrSwv1W&J904@W%nmWf++K99pRBvk&wBaNAs4JTid1Ji",
+  resave: false,
+  saveUninitialized: true
+}));
+
+//set the body object on every request. Can also be done on every request individually (when not all routes need the bodyparser)
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //setup security
 app.use(require('./security/security-setup'));
 
-//Serve the static content and require all the controllers
+//log all the requests
 app.use(logger('dev'));
-//uncomment when bodyparser works
-//app.use(bodyParser.urlencoded({ extended: false }));
+
+//Serve the static content and require all the controllers
 app.use(express.static('public'));
 app.use(require('./controllers'));
 
