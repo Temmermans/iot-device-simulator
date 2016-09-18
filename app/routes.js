60435b91f6c0b05912dd2c01ca71e1d840a6f270
Simon Temmerman <temmerman.simon@gmail.com>
2016-09-18 16:03:20 +0200

    Clean up the setup of app (remove mvc, use return function of require statement

---
+++ app/routes.js
@@ -0,0 +1,40 @@
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+     res.render("pages/index");
+  })
+
+  app.get('/:name', function(req, res, next){
+    res.send('Hi ' + req.params.name);
+  })
+
+  // Errorhandling =============================================================
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('pages/error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('pages/error', {
+      message: err.message,
+      error: {}
+    });
+  });
+}
--- server.js
+++ server.js
@@ -3,80 +3,36 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var exphbs  = require('express-handlebars');
+var helmet = require('helmet');
 var app = express();
+
+// configuration ===============================================================
+
+//setup security ===============================================================
+require('./lib/security-setup')(app, helmet)
+
+// set up our express application ==============================================
+app.use(logger('dev')); // log every request to the console
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //set handlebars as default templating engine
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
 //the default store is not for production usage. Install a store compatible with the db you are using from: https://github.com/expressjs/session#compatible-session-stores
-//this is also a dependency of csurf
+//this is also a dependency of csurf and passport
 // app.use(session({
 //   secret: "@lHJr+JrSwv1W&J904@W%nmWf++K99pRBvk&wBaNAs4JTid1Ji",
 //   resave: false,
 //   saveUninitialized: true
 // }));
 
-// use domains for better error handling
-app.use(function(req, res, next){
-    // create a domain for this request
-    var domain = require('domain').create();
-    // handle errors on this domain
-    domain.on('error', function(err){
-        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
-        try {
-            // failsafe shutdown in 5 seconds
-            setTimeout(function(){
-                console.error('Failsafe shutdown.');
-                process.exit(1);
-            }, 5000);
-
-            // disconnect from the cluster
-            var worker = require('cluster').worker;
-            if(worker) worker.disconnect();
-
-            // stop taking new requests
-            server.close();
-
-            try {
-                // attempt to use Express error route
-                next(err);
-            } catch(error){
-                // if Express error route failed, try
-                // plain Node response
-                console.error('Express error mechanism failed.\n', error.stack);
-                res.statusCode = 500;
-                res.setHeader('content-type', 'text/plain');
-                res.end('Server error.');
-            }
-        } catch(error){
-            console.error('Unable to send 500 response.\n', error.stack);
-        }
-    });
-
-    // add the request and response objects to the domain
-    domain.add(req);
-    domain.add(res);
-
-    // execute the rest of the request chain in the domain
-    domain.run(next);
-});
-
+// serve the static content ====================================================
 app.use(express.static(__dirname + '/public'))
 
-//log all the requests
-app.use(logger('dev'))
+// routes ======================================================================
+require('./app/routes.js')(app) // load our routes and pass in our app
 
-//set the body object on every request. Can also be done on every request individually (when not all routes need the bodyparser)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-//setup security
-app.use(require('./security/security-setup'))
-//require all the controllers
-app.use(require('./controllers/index'))
-//errorhandling middleware
-app.use(require('./controllers/errorhandling'))
-
-// export the app as a node module so the logic is encapsulated
+// export so bin/www can launch ================================================
 module.exports = app;
