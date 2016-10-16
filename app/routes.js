52f622c909ee92a5c6cd804ff1a38c02df964f1a
Simon Temmerman <temmerman.simon@gmail.com>
2016-10-16 15:59:22 +0200

    Add beter errorhandling

--- app/routes.js
+++ app/routes.js
@@ -23,21 +23,49 @@ module.exports = function(app) {
   // will print stacktrace
   if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
+      if (err.status === 404) {
+        res.status(404);
+        res.render('404', {
+          message: err.message,
+          error: err
+        });
+      } else if (err.status === 500) {
+        res.status(500);
+        res.render('500', {
+          message: err.message,
+          error: err
+        });
+      } else {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err
+        });
+      }
     });
-  }
+   }
 
   // production error handler
   // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
-    });
+    if (err.status === 404) {
+      res.status(404);
+      res.render('404', {
+        message: err.message,
+        error: {}
+      });
+    } else if (err.status === 500) {
+      res.status(500);
+      res.render('500', {
+        message: err.message,
+        error: {}
+      });
+    } else {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {}
+      });
+    }
   });
 };
